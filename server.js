4364bb184d25c0bee880641f7c7213bd8c462824
Darryl Subal <dsubal@att.net>
2023-04-19 16:57:44 -0400

    return notes and index html file

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ app.use(express.json());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // GET /notes should return the notes.html file
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 // GET * should return the index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
 
